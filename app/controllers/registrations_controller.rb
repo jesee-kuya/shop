5e0250b7412d8314ad5168a266e1a94ca04c7cb3
jkuya <kuyajesee@proton.me>
2025-07-23 02:25:38 +0300

    refactor: add registration controller implementation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,26 @@
 # This must be implemented.
 # sign_up_params and account_update_params
 class RegistrationsController < Devise::RegistrationsController
+  # before creating a new user, allow :name in the sanitizer
+  before_action :configure_sign_up_params, only: [:create]
+  # before updating an existing user, allow :name in the sanitizer
+  before_action :configure_account_update_params, only: [:update]
 
- private
- 
+  private
+
+  # Permit these params on sign up
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [:name, :email, :password, :password_confirmation]
+    )
+  end
+
+  # Permit these params on account update
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:name, :email, :password, :password_confirmation, :current_password]
+    )
+  end
 end
