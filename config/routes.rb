83048484a4a73f01980f95a16af1c3370de6e4a3
jkuya <kuyajesee@proton.me>
2025-07-23 03:37:26 +0300

    feat: add configaration for removing items in cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   resources :products
   resources :cart_items, except: [:new, :edit]
   
-  # Add cart route for convenience
-  get 'cart', to: 'cart_items#index'
+  # Cart routes
+  resource :cart, only: [:show] do
+    post   'add/:product_id',    to: 'carts#add_item',    as: :add_item
+    delete 'remove/:product_id', to: 'carts#remove_item', as: :remove_item
+    delete 'empty',              to: 'carts#empty',       as: :empty
+  end
+  
+  # Legacy cart route for convenience (redirects to new cart route)
+  get 'cart', to: 'carts#show'
 end
