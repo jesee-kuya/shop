27cfe8ac2e97168a5b0bd2033f84949f2738e042
jkuya <kuyajesee@proton.me>
2025-07-23 03:31:57 +0300

    fix: fix the cart view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def product_image_tag(product, version = :thumb, options = {})
+    if product.image.present?
+      image_tag(product.image_url(version), options)
+    else
+      # Fallback image or placeholder
+      image_tag("placeholder.png", options.merge(alt: "No image available"))
+    end
   end
+end
